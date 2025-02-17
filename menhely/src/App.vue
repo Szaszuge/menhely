9bb60cda4ee804a128303cde9d9591781350929d
kecskeszsoltistvan <78853814+kecskeszsoltistvan@users.noreply.github.com>
2025-02-17 18:43:58 +0100

    Navbar (Ikon hányzik)

--- menhely/src/App.vue
+++ menhely/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  
+  <Navbar/>
   <RouterView />
 </template>
 
---
+++ menhely/src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+<div id="main">
+    <div id="left">
+        <!-- TODO: SVG ikon betöltése -->
+    </div>
+    <div id="right">
+        <RouterLink to="/" class="link">Állatkatalógus</RouterLink>
+        <RouterLink to="/" class="link">Állat leadása</RouterLink>
+        <RouterLink to="/" class="link">Hogyan segíthet</RouterLink>
+        <RouterLink to="/" class="link">Bejelentkezés</RouterLink>
+    </div>
+</div>
+</template>
+
+<style scoped>
+#main {
+
+    background-color: var(--bg-color-weak);
+
+    max-height: 130px;
+    height: calc(50px + 10vh);
+    min-height: 70px;
+    
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#right {
+    font-weight: 600;
+    font-size: 29px;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+}
+
+#right:last-child{
+    margin-right: 35px;
+}
+
+.link{
+    margin: 0px 30px;
+}
+</style>
